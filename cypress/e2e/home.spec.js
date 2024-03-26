38d9c4a487e28a87f33bd8e469b602ed5b5841ec
Fran pc bichin <franciscolopto@gmail.com>
2024-03-26 14:21:54 +0100

    add context

--- cypress/e2e/home.spec.js
+++ cypress/e2e/home.spec.js
@@ -3,15 +3,19 @@ describe('home spec', () => {
     cy.visit('/')
     cy.wait(500)
   })
-  it('passes', () => {
-    cy.getByData('hero-heading').contains("Testing Next.js Applications with Cypress")
-  })
 
-  it('verificamos las caracteristicas de los cursos ', () => {
-    // cy.get('dt').eq(0).contains(/4 courses/i) expresiones regulares
-    cy.get('dt').eq(0).contains('4 Courses')
-    cy.get('dt').eq(1).contains('25+ Lessons')
-    cy.get('dt').eq(2).contains('Free and Open Source')
-    
+  context('Seccion inicial', () => {
+
+    it('passes', () => {
+      cy.getByData('hero-heading').contains("Testing Next.js Applications with Cypress")
+    })
+  
+    it('verificamos las caracteristicas de los cursos ', () => {
+      // cy.get('dt').eq(0).contains(/4 courses/i) expresiones regulares
+      cy.get('dt').eq(0).contains('4 Courses')
+      cy.get('dt').eq(1).contains('25+ Lessons')
+      cy.get('dt').eq(2).contains('Free and Open Source')
+      
+    })
   })
 })
